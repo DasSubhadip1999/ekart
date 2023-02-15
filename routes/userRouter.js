101136e6c0735430f13b25b22ce0567ec9fe6a93
DasSubhadip1999 <subhadipdasrng54@gmail.com>
2023-02-15 12:27:48 +0530

    validator added for req.body in user register

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,8 +1,20 @@
 const express = require("express");
+const { body } = require("express-validator");
 const { register } = require("../controllers/userController");
+const validator = require("../middlewares/validatorMiddleware");
 
 const userRouter = express.Router();
 
-userRouter.route("/register").post(register);
+userRouter
+  .route("/register")
+  .post(
+    body("name").exists({ checkFalsy: true }).withMessage("Please enter name"),
+    body("email").isEmail().withMessage("Please enter a valid email id"),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("Password should have minimum length of six"),
+    validator,
+    register
+  );
 
 module.exports = userRouter;
