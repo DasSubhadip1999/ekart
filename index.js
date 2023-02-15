47f4eb58499f66f02e2b58630fc8140b1307a8a0
DasSubhadip1999 <subhadipdasrng54@gmail.com>
2023-02-15 11:57:17 +0530

    User route for register create

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use("/users", require("./routes/userRouter"));
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to Ekart" });
