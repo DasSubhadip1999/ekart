bd2637face4a733d83f270dd557f058617973ec1
DasSubhadip1999 <subhadipdasrng54@gmail.com>
2023-02-15 15:05:49 +0530

    product router create

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,3 +3,7 @@ const asynchandler = require("express-async-handler");
 const addProduct = asynchandler(async (req, res) => {
   const { name, price } = req.body;
 });
+
+module.exports = {
+  addProduct,
+};
