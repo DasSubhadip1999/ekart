ee59a9332d6fda02c48c51e7c8242be318cf007b
DasSubhadip1999 <subhadipdasrng54@gmail.com>
2023-02-15 12:15:18 +0530

    db configuration create

---
+++ config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const DB = process.env.DB;
+const MONGO_URI = process.env.MONGO_URI;
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(`${MONGO_URI}/${DB}`);
+    console.log(`MongoDB connected to: ${conn.connection.host}`.cyan.underline);
+  } catch (error) {
+    console.log(`Error: ${error.message}`.red.bold);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- models/userModel.js
+++ models/userModel.js
@@ -5,10 +5,12 @@ const userSchema = mongoose.Schema(
     name: {
       type: String,
       required: true,
+      trim: true,
     },
     email: {
       type: String,
       required: true,
+      trim: true,
     },
     password: {
       type: String,
