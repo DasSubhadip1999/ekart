0035449874d353144736cecf2583aa5a6c388421
DasSubhadip1999 <subhadipdasrng54@gmail.com>
2023-02-15 19:57:34 +0530

    car model create

---
+++ models/cartModel.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const cartSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  products: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+    },
+  ],
+  expectedDeliveryDate: {
+    type: Date,
+    default: () => new Date(new Date().setDate(new Date().getDate() + 7)),
+  },
+});
+
+module.exports = mongoose.model("Cart", cartSchema);
