c8ffb69c3d11f54565515ac0498584b7b8f6d7e0
DasSubhadip1999 <subhadipdasrng54@gmail.com>
2023-02-15 11:53:01 +0530

    express app initialize

---
+++ index.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+require("colors");
+const express = require("express");
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to Ekart" });
+});
+
+app.listen(PORT, () =>
+  console.log(`Server started on port ${PORT}`.yellow.underline)
+);
