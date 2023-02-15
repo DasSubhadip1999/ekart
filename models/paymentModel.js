a7352f05873612d7bc2ca9cc1f09d89f02d61a86
DasSubhadip1999 <subhadipdasrng54@gmail.com>
2023-02-15 23:52:26 +0530

    payment model create

--- models/paymentModel.js
+++ models/paymentModel.js
@@ -1,1 +1,27 @@
 const mongoose = require("mongoose");
+
+const paymentSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  products: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+    },
+  ],
+  paymentAmount: {
+    type: Number,
+    required: true,
+  },
+  paymentMode: {
+    type: String,
+    enum: ["online", "cod"],
+  },
+  paymentStatus: {
+    type: String,
+    enum: ["pending", "fullfilled"],
+  },
+});
